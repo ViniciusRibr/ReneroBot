2ee4eab5790237b25cc663171a6eedec44233155
ViniciusRibr <viniciusrib200@gmail.com>
2024-10-14 16:19:28 -0300

    1.2
    Adição de loop e de novas saudações e despedidas.

--- Interface.py
+++ Interface.py
@@ -4,13 +4,25 @@ from Perguntas import interacaoChatBot
 teste = interacaoChatBot()
 print(teste.iteracao_ram())
 
-# Entrada do usuário
-prompt = input("Digite sua pergunta: ")
+while True:
+    # Entrada do usuário
+    prompt = input("Digite sua pergunta, (Ou digite 'sair' para encerrar.): ")
     
-# Gerar resposta do chatbot
-conversa = GerarConteudo(prompt)
-textogerado = conversa.gerarTexto()
-print(textogerado)
+    if prompt.lower() == 'sair':
+        print(teste.say_goodbye())  # Exibe a despedida
+        break  # Encerra o loop
+
+    # Gerar resposta do chatbot
+    try:
+        conversa = GerarConteudo(prompt)
+        textogerado = conversa.gerarTexto()
+        print(textogerado)
+
+        # Verifica se há uma resposta válida antes de imprimir
+        if textogerado:
+            print(textogerado)
+        else:
+            print("Não foi possível gerar uma resposta.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao gerar a resposta: {e}")
     
-# Despedida
-print(teste.say_goodbye())
