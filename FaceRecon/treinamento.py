3dd687645cde58657e4c50054596bbd12caf2577
Ryan478990 <163213677+Ryan478990@users.noreply.github.com>
2024-11-19 18:29:31 -0300

    FaceRecon 1.0

--- FaceRecon/treinamento.py
+++ FaceRecon/treinamento.py
@@ -1,35 +1,28 @@
 import cv2
-import os
-import numpy as np
 
-eigeface = cv2.face.EigenFaceRecognizer_create(num_components=50)
-#fisherface = cv2.face.FisherFaceRecognizer_create()
-#lbph = cv2.face.LBPHFaceRecognizer_create()
+classific =  cv2.CascadeClassifier("haarcascade_frontalface_default.xml")#Classificador para encontrar faces
+reconhecedor = cv2.face.EigenFaceRecognizer_create()
+reconhecedor.read("classificadorEigen.yml")#Classificador treinado para identificar a face do estudante
+largura, altura = 220, 220
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+camera = cv2.VideoCapture(0)
 
-def getImagensComId():
-    caminhos = [os.path.join('fotos',f) for f in os.listdir('fotos')]
-    #print(caminhos)
-    faces = []
-    ids = []
-    for caminhoImagem in caminhos:
-        imagemFace = cv2.cvtColor(cv2.imread(caminhoImagem), cv2.COLOR_BGR2GRAY)
-        id = int(os.path.split(caminhoImagem)[-1].split('.')[1])
-        #print(id)
-        ids.append(id)
-        faces.append(imagemFace)
-        #cv2.imshow("Face", imagemFace)
-        #cv2.waitKey(10)
-    return np.array(ids), faces
-ids, faces = getImagensComId()
-print("Treinando...")
 
-eigeface.train(faces, ids)
-eigeface.write('classificadorEigen.yml')
+while (True):
+#Detecção das faces
+    conectado, imagem = camera.read()
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    facesdetec = classific.detectMultiScale(imagemCinza, scaleFactor=1.5,minSize=(150,150))
+#Criação de um retangulo onde foi encontrada a face do estudante
+    for (x, y, l, a) in facesdetec:
+        imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+        cv2.rectangle(imagem, (x,y), (x + l, y + a), (0,0,255))
+        id, confianca = reconhecedor.predict(imagemFace)
+        cv2.putText(imagem, str(id), (x,y + (a+30)), font, 2, (0,0,255))
+    cv2.imshow("Face", imagem)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-#fisherface.train(faces, ids)
-#fisherface.write('classificadorFisher.yml')
 
-#lbph.train(faces, ids)
-#lbph.write('classificadorlbph.yml')
-
-print("Treinamento realizado!")
+camera.release()
+cv2.destroyAllWindowns()
